facaa28c7a4524f38780880036d8381728ec6360
botnyx <chlsbkrs@gmail.com>
2019-03-07 11:56:23 +0100

    ..

--- src/includes/container.php
+++ src/includes/container.php
@@ -1,1 +1,45 @@
 <?php
+
+
+$container['frontendconfig'] = function($c){
+
+    	/*
+    		if frontend is enabled, serve it...  else 403
+    	*/
+    	//if(array_key_exists('sfeFrontend',_SETTINGS)){
+
+    	// frontend remote-Config
+
+    	// Create default HandlerStack
+    	$stack = GuzzleHttp\HandlerStack::create();
+    	$stack->push(
+    		  new CacheMiddleware(
+    			new PrivateCacheStrategy(
+    			  new DoctrineCacheStorage(
+    				new FilesystemCache(_SETTINGS['paths']['temp'].'/guzl')
+    			  )
+    			)
+    		  ),
+    		  'cache'
+    		);
+
+    	$headers = ['referer' => 'https://'._SETTINGS['paths']['fdqn'],'origin' => 'https://'._SETTINGS['paths']['fdqn'] ];
+
+    	$cachedClient = new GuzzleHttp\Client([
+    		'headers' => $headers,
+    		'handler' => $stack
+    	]);
+
+    	try{
+    		$res = $cachedClient->request('GET', _SETTINGS['sfeFrontend']['sfeBackend'].'/api/cfg/'._SETTINGS['sfeFrontend']['clientId']);
+    		$frontEndConfig = json_decode($res->getBody());
+    	}catch(Exception $e){
+    		die($e->getMessage());
+    	}
+
+
+
+
+
+    	return $frontEndConfig;
+};
--- src/includes/routes.php
+++ src/includes/routes.php
@@ -1,6 +1,66 @@
 <?php
 
 
+use Slim\Http;
+use Slim\Views;
+
+
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\Common\Cache\PredisCache;
+
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
+use Kevinrob\GuzzleCache\Strategy\PublicCacheStrategy;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
+
+use Kevinrob\GuzzleCache\KeyValueHttpHeader;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
+
+
+
+
+//die(_SETTINGS['sfeFrontend']['clientId']);
+
+foreach($container['frontendconfig']->routes as $route){
+	$app->get( $route->uri,$route->fnc );
+
+}
+//$frontEndConfig['routes'];
+
+
+$app->get('/expiretest',  function ( $request,  $response, array $args){
+
+		// https://backend.devpoc.nl/api/cfg/myclientId
+
+		$res = $response->write("justtesting");
+		//$resWithExpires = $this->cache->withExpires($res, time() + 3600);
+		$res = $this->cache->withExpires($res, time() + 3600);
+		$resWithLastMod = $this->cache->withLastModified($res, time() - 3600);
+
+
+		return $resWithLastMod;
+
+	})->add(new \Slim\HttpCache\Cache('public', 86400));;
+
+
+
+
+	/* the Homepage */
+	$app->get('zz/',  function ( $request,  $response, array $args){
+
+
+
+		return $this->view->render($response, 'index.html', _SETTINGS['sfeFrontend'] );
+		//return $response->write('“There is nothing more important than a good, safe, secure home.”');
+	});
+
+
+
 
 
 $app->get('/robots.txt',  function ( $request,  $response, array $args){
