9d4d22f71ef73c8dd216cb7ebad269ead671d43d
botnyx <chlsbkrs@gmail.com>
2019-03-12 20:31:50 +0100

    ..

--- src/Core/WebAssets/FontEndpoint.php
+++ src/Core/WebAssets/FontEndpoint.php
@@ -3,16 +3,49 @@
 
 namespace Botnyx\Sfe\Frontend\Core\WebAssets;
 
+use Interop\Container\ContainerInterface;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\Common\Cache\PredisCache;
 
 class FontEndpoint{
 	
 	function __construct(ContainerInterface $container){
-		$this->assetProxy = new Botnyx\Sfe\Frontend\Core\WebAssets\AssetProxy($container);
+		$this->cacher = $container->get('cache');
+		
+		$this->assetProxy = new \Botnyx\Sfe\Frontend\Core\WebAssets\AssetProxy($container);
 	}
 	
 	function get(ServerRequestInterface $request, ResponseInterface $response, array $args = []){
 		
+		//$parsedPath = $this->parsePath($args['path'],$request->getAttributes('route')['routeInfo']);
+		
+		//$this->assetProxy->get("/_/assets/js/".$args['path']);
+		//$this->assetProxy->get("/_/assets/fonts/".$args['path']);
+		
+		try{
+			$returnedData = $this->assetProxy->get("/_/assets/fonts/".$args['path']);		
+		}catch(\Exception $e){
+			if($e->getCode()==404){
+				return $this->assetProxy->e404($response);
+				//return $response->withStatus(404);
+			}
+			//$e->getCode();
+			
+		}
+		
+		
+		$res = $response->write( $returnedData['html'] )->withHeader('Content-Type',$returnedData['Content-Type']);
+		//$resWithExpires = $this->cache->withExpires($res, time() + 3600);
+		$responseWithCacheHeader = $this->cacher->withExpires($res, time() + 3600);
+		$responseWithCacheHeader = $this->cacher->withLastModified($responseWithCacheHeader, $returnedData['Last-Modified'] );
+		return $responseWithCacheHeader;
+		
 	}
+	
 	
 }
 
