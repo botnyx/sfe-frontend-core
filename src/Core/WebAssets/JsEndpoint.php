fe55fdbf23a7fadf7af9eb400f596146265a774f
botnyx <chlsbkrs@gmail.com>
2019-03-14 14:09:02 +0100

    Update JsEndpoint.php

--- src/Core/WebAssets/JsEndpoint.php
+++ src/Core/WebAssets/JsEndpoint.php
@@ -14,60 +14,62 @@ use Doctrine\Common\Cache\PredisCache;
 
 
 class JsEndpoint{
-	
-	
+
+
 	function __construct(ContainerInterface $container){
 		$this->cacher = $container->get('cache');
-		
+
 		$this->assetProxy = new \Botnyx\Sfe\Shared\WebAssets\AssetProxy($container);
-		
+
 		$this->client_id = $container->get("settings")['sfe']['clientId'];
-		
+
 		$this->allowOrigin = $container->get("settings")['paths']['url'];
-		
+
 		$this->expireTime = time() + (3600*24)*357;
 	}
-	
+
 	function get(ServerRequestInterface $request, ResponseInterface $response, array $args = []){
-		
+
 		//print_r($this->settings['sfe']['clientId']);
 		//die();
-		
+
 		try{
-			$res =  $this->assetProxy->get($response, _SETTINGS['sfeFrontend']['sfeBackend']."/_/assets/".$this->client_id."/js/".$args['path']);		
+			$res =  $this->assetProxy->get($response, _SETTINGS['sfeFrontend']['sfeBackend']."/_/assets/".$this->client_id."/js/".$args['path']);
 		}catch(\Exception $e){
 			if($e->getCode()==404){
 				return $this->assetProxy->e404($response)->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);;
 				//return $response->withStatus(404);
 			}else{
+				return $this->assetProxy->e500($response)->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);;
+
+				//die();
 				//return $this->assetProxy->e404($response)->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);;
 				//throw new \Exception( $e->getMessage(), $e->getCode() );
 				//print_r($e->getBody());
 				#print_r($e->getCode());
 				#print_r($e->getMessage());
-				return $this->assetProxy->e500($response)->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);;
 				//$response = \Botnyx\Sfe\Shared\ExceptionResponse::get($response,$e->getCode(),'Cdn reports: 404 Not Found');
 				//return $response;
 				//die("xxx");
 				//
-				return $response->withStatus( $e->getCode() );//->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);;
+
+				//return $response->withStatus( $e->getCode() );//->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);;
 			}
-			
-			
+
+
 		}
-		
-		
+
+
 		/*
 			Set the cache headers.
 		*/
 		$res = $res->withHeader('Cache-Control','public');
 		$res = $res->withHeader('Pragma','public');
 		$res = $this->cacher->withExpires($res, $this->expireTime);
-		
-		
-		return $res->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);
-		
-	}
-	
-}
 
+
+		return $res->withHeader('Access-Control-Allow-Origin',$this->allowOrigin);
+
+	}
+
+}
