1f5776ec1320e049a13db1761de47b8078796ada
botnyx <chlsbkrs@gmail.com>
2019-03-18 13:24:52 +0100

    ..

---
+++ src/Core/Objects/config/Frontend.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Botnyx\Sfe\Frontend\Core\Objects\config;
+
+//namespace Botnyx\Sfe\Backend\Core;
+
+class Frontend {
+	
+	var $clientid="";
+	var $clientsecret="";
+	var $conn;
+	
+	function __construct($settings){
+		
+		
+		if(!array_key_exists('clientId',$settings['sfeFrontend'])){
+			throw new \Exception("Fatal Error in Configuration.ini : Missing `clientId` in the `sfeFrontend` section.");
+		}
+		
+		$this->clientId = $settings['sfeFrontend']['clientId'];
+		
+		
+		if(array_key_exists('clientSecret',$settings['sfeBackend'])){
+			$this->clientsecret = new \Botnyx\Sfe\Shared\ProtectedValue($settings['sfeBackend']['clientSecret']);
+		}
+		
+		
+		
+		$hosts = new Botnyx\Sfe\Shared\Objects\config\SfeHosts();
+			
+		if(!array_key_exists('sfeCdn',$settings['sfeFrontend'])){
+			throw new \Exception("Fatal Error in Configuration.ini : Missing `sfeCdn` in the `sfeFrontend` section.");
+		}
+		$hosts->cdn = $settings['sfeFrontend']['sfeCdn'];
+		
+		
+		
+		if(!array_key_exists('sfeBackend',$settings['sfeFrontend'])){
+			throw new \Exception("Fatal Error in Configuration.ini : Missing `sfeBackend` in the `sfeFrontend` section.");
+		}
+		$hosts->backend = $settings['sfeFrontend']['sfeBackend'];
+		
+		
+		
+		if(!array_key_exists('sfeAuth',$settings['sfeFrontend'])){
+			throw new \Exception("Fatal Error in Configuration.ini : Missing `sfeAuth` in the `sfeFrontend` section.");
+		}
+		$hosts->auth = $settings['sfeFrontend']['sfeAuth'];
+		
+
+	}
+	
+	
+}
