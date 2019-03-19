9a4953aee789da830c626512cfb319e952bbde0c
botnyx <chlsbkrs@gmail.com>
2019-03-19 23:16:53 +0100

    ..

--- src/includes/container.php
+++ src/includes/container.php
@@ -16,11 +16,56 @@ use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
 //use Kevinrob\GuzzleCache\KeyValueHttpHeader;
 //use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
 
+$container['cache'] = function ($c) {
+    return new \Slim\HttpCache\CacheProvider();
+};
+
+
+$container['frontendconfig'] = function($c){
+
+    	/*
+    		if frontend is enabled, serve it...  else 403
+    	*/
+    	//if(array_key_exists('sfeFrontend',_SETTINGS)){
+    	// frontend remote-Config
+    	// Create default HandlerStack
+    	$stack = GuzzleHttp\HandlerStack::create();
+    	$stack->push(
+    		  new \Kevinrob\GuzzleCache\CacheMiddleware(
+    			new \Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy(
+    			  new \Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage(
+    				new \Doctrine\Common\Cache\FilesystemCache(_SETTINGS['paths']['temp'].'/guzl')
+    			  )
+    			)
+    		  ),
+    		  'cache'
+    		);
+
+    	$headers = ['referer' => 'https://'._SETTINGS['paths']['fdqn'],'origin' => 'https://'._SETTINGS['paths']['fdqn'] ];
+
+    	$cachedClient = new GuzzleHttp\Client([
+    		'headers' => $headers,
+    		'handler' => $stack
+    	]);
+
+    	try{
+    		$res = $cachedClient->request('GET', _SETTINGS['sfeFrontend']['sfeBackend'].'/api/cfg/'._SETTINGS['sfeFrontend']['clientId']);
+    		$frontEndConfig = json_decode($res->getBody());
+    	}catch(Exception $e){
+    		die($e->getMessage());
+    	}
+		
+		#echo "<pre>";
+		#print_r($frontEndConfig);
+		#die();
+
+
+    	return $frontEndConfig->data;
+};
 
 
 
 //Override the default Not Found Handler before creating App
-/*
 $container['notFoundHandler'] = function ($c) {
     return function ($request, $response) use ($c) {
         $view = new \Slim\Views\Twig(_SETTINGS['paths']['root'].'/vendor/botnyx/sfe-shared-core/templates/errorPages', [
@@ -55,7 +100,7 @@ $container['phpErrorHandler'] = function ($c) {
 		return $response->withStatus(500)->withHeader('Content-Type', 'text/html')->write('CUSTOM Something went wrong!');
     };
 };
-
+/*
 */
 
 
--- src/includes/routes.php
+++ src/includes/routes.php
@@ -26,8 +26,42 @@ use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
 
 //die(_SETTINGS['sfeFrontend']['clientId']);
 
+foreach($container['frontendconfig']->routes as $route){
+	$app->get( $route->uri,$route->fnc );
+
+}
+//$frontEndConfig['routes'];
+
+$app->get( '/a/js/[{path:.*}]',    '\\Botnyx\\Sfe\\Frontend\\Core\\WebAssets\\JsEndpoint:get' );
+$app->get( '/a/css/[{path:.*}]',   '\\Botnyx\\Sfe\\Frontend\\Core\\WebAssets\\CssEndpoint:get' );
+$app->get( '/a/fonts/[{path:.*}]', '\\Botnyx\\Sfe\\Frontend\\Core\\WebAssets\\FontEndpoint:get' );
+
+
+$app->get('/expiretest',  function ( $request,  $response, array $args){
+
+		// https://backend.devpoc.nl/api/cfg/myclientId
+
+		$res = $response->write("justtesting");
+		//$resWithExpires = $this->cache->withExpires($res, time() + 3600);
+		$res = $this->cache->withExpires($res, time() + 3600);
+		$resWithLastMod = $this->cache->withLastModified($res, time() - 3600);
+
+
+		return $resWithLastMod;
+
+})->add(new \Slim\HttpCache\Cache('public', 86400));;
 
 
 
 
 
+
+
+
+
+
+$app->get('/robots.txt',  function ( $request,  $response, array $args){
+  $res = "User-agent: *".PHP_EOL."Disallow: /";
+  return $response->write($res);
+
+});
