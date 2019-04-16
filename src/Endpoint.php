395407487bcb349c16c29c60289c23af88bdd493
botnyx <chlsbkrs@gmail.com>
2019-04-16 16:59:12 +0200

    Update Endpoint.php

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -206,5 +206,67 @@ class Endpoint {
 
 		//return $response->write("xx");
 	}
+	
+	public function oauthProxy(ServerRequestInterface $request, ResponseInterface $response, array $args = []){
+		
+		// Initialize the client with the handler option
+		$client = new \GuzzleHttp\Client([
+			/*'handler' => $this->_stack,*/
+			'http_errors'=>true
+		]);
+		
+		$extraoptions =  [
+			'auth' => ['username', 'password'],
+			'form_params' => [
+				'grant_type' => 'authorization_code',
+				'code' => '123',
+				'redirect_uri' => ''
+			]
+		];
+		
+		$res = $this->client->request('POST', $Backend.'/api/sfe/'.$Clientid.'/uri/'.$language.'/'.$theEndpointId."?".http_build_query($args) ,$extraoption );
+		
+		
+$clientid="b75d3c0b-ae2a-420b-9635-c910b3ec4ed8";
+$clientsecret="dCHQiWosJn79u_UrFF1YmrTW-a2VbWCtjc5gNbcpGhY";
 
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://account.trustmaster.org/oauth2/token");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_USERPWD, $clientid . ":" . $clientsecret);  
+
+$data = array(
+    'grant_type' => 'authorization_code',
+    'code' => $_GET['code'],
+	'redirect_uri' => $_GET['redirect_uri']
+);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$output = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);	
+
+$x = json_decode($output);
+
+header("Location: ".$_GET['redirect_uri']."#access_token=".$x->access_token."&state=".urlencode($_GET['state']));
+
+		
+		
+		
+		$Backend = "https://".$this->sfe->hosts->backend;
+		$Clientid = $this->sfe->clientid;
+
+
+		$res = $this->client->request('GET', $Backend.'/api/sfe/'.$Clientid.'/ui/sw');
+
+
+		return $response->write($res->getBody())->withHeader("content-type","application/javascript; charset=utf-8");
+
+
+		//return $response->write("xx");
+	}
+	
+	
 }
